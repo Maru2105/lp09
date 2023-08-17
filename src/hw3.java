ba550b4dffd3b3cf644622a447dab418f592a54d
Maria Troshina <maru2105@yandex.ru>
2023-08-17 19:55:44 +0300

    сделали повтор действий и вывод конец игры

--- src/hw3.java
+++ src/hw3.java
@@ -1,31 +1,42 @@
 import java.util.Scanner;
 
 public class hw3 {
+    static int number;
+    static int attempts;
+
     public static int getRandomDiceNumber() {
         return (int) (Math.random() * 9);
     }
 
     public static void main(String[] args) {
         int x = getRandomDiceNumber();
+        System.out.println(x);
+        attempts = 0;
+        do {
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите цифру от 0 до 9");
-        int number = (int) in.nextFloat();
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите цифру от 0 до 9");
+            number = in.nextInt();
 
-        if (x == number) {
-            System.out.println("верно");
-        }
-        if (x > number || x < number) {
-            System.out.println("неверно");
-
-            if (number > x) {
-                System.out.println("меньше");
+            if (x == number) {
+                System.out.println("верно");
             }
+            if (x > number || x < number) {
+                System.out.println("неверно");
+
+                if (number > x) {
+                    System.out.println("меньше");
+                }
                 if (number < x) {
                     System.out.println("больше");
                 }
             }
+            attempts++;
+        } while (x!= number && attempts <3);
+        System.out.println("End");
 
-        }
+
+
     }
 
+}
