6a4010031cc4b50e8741b8baa021a57d7649cdf2
Linka <Polina153@mail.ru>
2023-08-18 12:24:30 +0400

    1 fix new method 2 3 attempts 3 if works well => new branch "two_games_in_one" 4 adding new class 5 fixing Main

--- src/hw3.java
+++ src/hw3.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class hw3 {
     static int number;
-    static int world;
     static boolean gameIsOn;
 
     public static int getRandomDiceNumber() {
         return (int) (Math.random() * 9);
     }
-
+//TODO add this code to new class Game3attempts. In Main (Hw3) - starting game + asking to play again
     public static void main(String[] args) {
         int x = getRandomDiceNumber();
         System.out.println(x);
@@ -18,8 +17,6 @@ public class hw3 {
             Scanner in = new Scanner(System.in);
             System.out.println("Введите цифру от 0 до 9");
             number = in.nextInt();
-
-
             if (x == number) {
                 System.out.println("верно");
                 gameIsOn = false;
@@ -27,20 +24,17 @@ public class hw3 {
             if (x > number || x < number) {
                 System.out.println("неверно");
                 attempts--;
-
             }
-                   if (number > x) {
-                       System.out.println("меньше");
-
-                       if (number < x) {
-                           System.out.println("больше");
-                       }
-                       System.out.println("Осталось попыток" + " " + attempts);
-                   }
-                if(attempts <=-0){
-                    break;
-
+            if (number > x) {
+                System.out.println("меньше");
+                if (number < x) {
+                    System.out.println("больше");
                 }
+                System.out.println("Осталось попыток" + " " + attempts);
+            }
+            if (attempts <= 0) {
+                gameIsOn = false;
+            }
 
         } while (gameIsOn);
         System.out.println("End");
@@ -48,14 +42,17 @@ public class hw3 {
 
         Scanner yesno = new Scanner(System.in);
         System.out.println("Введите число");
-        int y = yesno.nextInt();
-        if (y == 1){
-           //как сделать повтор нужного цикла
+        int y = yesno.nextInt();//TODO switch instead of if
+        if (y == 1) {
+            //как сделать повтор нужного цикла
+            //TODO gameCircle - in separate method
+            //FIXME new random number every attempt (check 3 tries)
         }
-        if (y == 0){
+        if (y == 0) {
             System.out.println("Спасибо за игру");
-        }while (gameIsOn);
-
         }
+        while (gameIsOn) ;
+
     }
+}
 
